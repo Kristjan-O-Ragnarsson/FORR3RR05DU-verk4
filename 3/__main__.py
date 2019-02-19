daab4d1441a3ac8aa46d482fda9cca397cb3a5b0
Kristjan-O-Ragnarsson <kristjanra262@nemi.tskoli.is>
2019-02-19 12:44:37 +0000

    fixed it

--- 3/__main__.py
+++ 3/__main__.py
@@ -1,3 +1,5 @@
+import sys
+
 _l = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 
@@ -5,15 +7,19 @@ def bins(li, n):
     mid = len(li) // 2
     #print li
     #print mid
+    if mid == 0:
+        return -1
     if li[mid] == n:
         return _l.index(n)
     elif li[mid] < n:
         return bins(li[mid:], n)
     elif li[mid] > n:
-        for i in range(len(li[1:])):
-            if li[i + 1] == n:
+        for i in range(len(li[:mid])):
+            if li[i] == n:
+                print "kristjan"
                 return _l.index(n)
+        print "kristjan"
         return -1
 
 
-print bins(_l, 4)
+print bins(_l, 10)
